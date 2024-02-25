4b59e5569dd69e92ef13c366a1501f237c4390eb
CatalinaScheleff <catascheleff@gmail.com>
2024-02-25 17:43:06 -0300

    Create User
    email and password

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,6 +1,10 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import React from 'react';
+import { auth } from '@/firebase/firebase';
+import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
+
 
 type SignupProps = {
     
@@ -12,15 +16,40 @@ const Signup:React.FC<SignupProps> = () => {
     const handleClick = () => {
         setAuthModalState((prev) => ({ ...prev, type:"login"}))
     }
+
+    const [inputs, setInputs] = useState({email: '', displayName: '', password: ''})
+    const router = useRouter();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
+    };
+    const handleRegister = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+            if (!newUser) return;
+            router.push('/')
+        } catch (error:any) {
+            alert(error.message)
+        }
+    }
+    
+
     
     return (
-        <form className='space-y-6 px-6 pb-4'>
+        <form className='space-y-6 px-6 pb-4' onSubmit={handleRegister}>
         <h3 className='text-xl font-medium text-white'>Register on LeetClone</h3>
         <div>
             <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
                  Email
             </label>
-            <input type='email' name='email' id='email' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+            <input onChange={handleChangeInput} type='email' name='email' id='email' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
             placeholder='name@company.com'
             />
         </div>
@@ -28,7 +57,7 @@ const Signup:React.FC<SignupProps> = () => {
             <label htmlFor='displayName' className='text-sm font-medium block mb-2 text-gray-300'>
                 Display Name
             </label>
-            <input type='displayName' name='displayName' id='displayName' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+            <input onChange={handleChangeInput} type='displayName' name='displayName' id='displayName' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
             placeholder='John Doe'
             />
         </div>
@@ -36,7 +65,7 @@ const Signup:React.FC<SignupProps> = () => {
             <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
                  Password
             </label>
-            <input type='password' name='password' id='password' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+            <input onChange={handleChangeInput} type='password' name='password' id='password' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
             placeholder='*******'
             />
         </div>
