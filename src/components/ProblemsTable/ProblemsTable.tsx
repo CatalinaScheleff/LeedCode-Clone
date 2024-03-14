c46630042ff689222c70064138b20ba4cc3fcc75
CatalinaScheleff <catascheleff@gmail.com>
2024-03-14 15:58:33 -0300

    youtubeModal

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,8 +1,10 @@
 import { problems } from '@/mockProblems/problems.';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCheckCircle } from 'react-icons/bs'
 import { AiFillYoutube } from 'react-icons/ai'
+import YouTube from 'react-youtube';
+import { IoClose } from 'react-icons/io5';
 
 type ProblemsTableProps = {
 
@@ -10,37 +12,69 @@ type ProblemsTableProps = {
 
 const ProblemsTable: React.FC<ProblemsTableProps> = () => {
 
+    const [youtubePlayer, setYoutubePlayer] = useState({
+        isOpen: false,
+        videoId: ""
+    })
+
+    const closeModal = () => {
+        setYoutubePlayer({
+            isOpen: false,
+            videoId: ""
+        })
+    }
+
     return (
-        <tbody className='text-white'>
-            {problems.map((doc, index) => {
+        <>
+            <tbody className='text-white'>
+                {problems.map((doc, index) => {
 
-                const difficultyColor = doc.difficulty === "Easy" ? "text-dark-green-s" : doc.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink"
+                    const difficultyColor = doc.difficulty === "Easy" ? "text-dark-green-s" : doc.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink"
 
-                return (
-                    <tr className={`${index % 2 !== 0 ? 'bg-dark-layer-1' : ''}`} key={doc.id}>
-                        <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
-                            <BsCheckCircle fontSize={"18"} width={"18"} />
-                        </th>
-                        <td className='px-6 py-4'>
-                            <Link className='hover:text-blue-600 cursor-pointer' href={`/problems/${doc.id}`}>
-                                {doc.title}
-                            </Link>
-                        </td>
-                        <td className={`px-6 py-4 ${difficultyColor}`}>
-                            {doc.difficulty}
-                        </td>
-                        <td className={"px-6 py-4"}>
-                            {doc.category}
-                        </td>
-                        <td className={"px-6 py-4"}>
-                            {doc.videoId ? (
-                            <AiFillYoutube fontSize={"28"} className='cursor-pointer hover:text-red-500'/>
-                            ) : (<p className='text-gray-400'>Coming soon</p>)}
-                        </td>
-                    </tr>
-                )
-            })}
-        </tbody>
+                    return (
+                        <tr className={`${index % 2 !== 0 ? 'bg-dark-layer-1' : ''}`} key={doc.id}>
+                            <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
+                                <BsCheckCircle fontSize={"18"} width={"18"} />
+                            </th>
+                            <td className='px-6 py-4'>
+                                <Link className='hover:text-blue-600 cursor-pointer' href={`/problems/${doc.id}`}>
+                                    {doc.title}
+                                </Link>
+                            </td>
+                            <td className={`px-6 py-4 ${difficultyColor}`}>
+                                {doc.difficulty}
+                            </td>
+                            <td className={"px-6 py-4"}>
+                                {doc.category}
+                            </td>
+                            <td className={"px-6 py-4"}>
+                                {doc.videoId ? (
+                                    <AiFillYoutube fontSize={"28"} className='cursor-pointer hover:text-red-500' 
+                                    onClick={() => setYoutubePlayer({isOpen: true, videoId: doc.videoId as string})}
+                                    />
+                                ) : (<p className='text-gray-400'>Coming soon</p>)}
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+            {youtubePlayer.isOpen && (
+
+                <tfoot className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center'>
+                <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onClick={closeModal}></div>
+                <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
+                    <div className='w-full h-full flex items-center justify-center relative'>
+                        <div className='w-full relative'>
+                            <IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 rigth-0' 
+                            onClick={closeModal}
+                            />
+                            <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
+                        </div>
+                    </div>
+                </div>
+            </tfoot>
+                )}
+        </>
     )
 }
 export default ProblemsTable;
